9df1cd2b023e04268f690bac0e25e587fa24f6f2
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-02-22 00:08:38 +0300

    feat: implement `dateSample` function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string') {
+    const activityValue = Number.parseFloat(sampleActivity);
+    return Boolean(activityValue) && (activityValue > 0 && activityValue < MODERN_ACTIVITY) && 
+      Math.ceil(Math.log(MODERN_ACTIVITY / activityValue) / (0.693 / HALF_LIFE_PERIOD));
+  }
+  return false;
 };
