f42ef8519057cf02580f763ed8cff9bdfee78f99
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-02-21 16:28:07 +0300

    feat: implement `countCats` function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let catsTotal = 0;
+  backyard.forEach(el => {
+    catsTotal += el.filter(el => el === '^^').length;
+  });
+  return catsTotal;
 };
