919ba4e8c6de4c8d80d042c2bbbb37af581526db
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-02-22 19:50:56 +0300

    feat: implement `transform` function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    const res = [];
+    if (!Array.isArray(arr)) {
+      throw TypeError('Wrong type!');
+    } else {
+      for (let i = 0, len = arr.length; i < len; i++) {
+        switch (arr[i]) {
+          case '--discard-next': {
+            i++
+            break;
+          }
+          case '--discard-prev': {
+            res.pop();
+            break;
+          }
+          case '--double-next': {
+            if (i + 1 < len ) res.push(arr[i + 1]);
+            break;
+          }
+          case '--double-prev': {
+            if (i - 1 >= 0) res.push(arr[i - 1]);
+            break;
+          }
+          default: {
+            res.push(arr[i]);
+          }
+        }
+      }
+    }
+    return res;
 };
