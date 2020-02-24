df18ceb494075f8cdf6d0058b51247834196fbed
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-02-24 19:10:01 +0300

    feat: implement `DepthCalculator` class

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (arr.length === 0) {
+            return 1;
+        } else {
+            let maxDepth = 0;
+            let branchDepth = 0;
+            for (const innerArr of arr.filter(el => Array.isArray(el))) {
+                branchDepth = this.calculateDepth(innerArr);
+                if (maxDepth < branchDepth) {
+                    maxDepth = branchDepth;
+                }
+            }
+            return maxDepth + 1;
+        }
     }
 };
