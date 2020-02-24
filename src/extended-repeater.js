256ef818a8aa5a08172325f1147884afde816635
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2020-02-24 20:31:00 +0300

    feat: implement `repeater` function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,23 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const stringRepeatNumber = options.repeatTimes || 1;
+  const additionRepeatNumber = options.additionRepeatTimes || 1;
+  const addition = (options.addition !== undefined) ? options.addition : '';
+  const separator = options.separator || '+';
+  const addittionSeparator = options.additionSeparator || '|';
+  const result = [];
+
+  for (let i = 0; i < stringRepeatNumber; i++) {
+    result.push(`${str}`);
+    for (let j = 0; j < additionRepeatNumber; j++) {
+      result.push(`${addition}`);
+      if (additionRepeatNumber - j > 1) {
+        result.push(addittionSeparator);
+      }
+    }
+    if (stringRepeatNumber - i > 1) {
+      result.push(separator);
+    }
+  }
+  return result.join('');
 };
   
